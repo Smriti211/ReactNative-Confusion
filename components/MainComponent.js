155b091d9ffab402a2c7a55284656c2e6bd7c759
Smriti Garg <smriti2111999@gmail.com>
2020-06-24 15:41:13 +0530

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'; 
 import Menu from './MenuComponent';
-import DishDetail from './Dishdetailcomponent';
+import DishDetail from './DishdetailComponent';
 import {View,Platform,StatusBar} from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Home from './HomeComponent';
 
 const MenuNavigator=createStackNavigator();
 function MyStack() {
@@ -19,7 +21,44 @@ function MyStack() {
                 options={{ title: 'Menu' }}/>
             <MenuNavigator.Screen name="DishDetail" component={DishDetail} 
                 options={{ title: 'Dish Details' }}/>
+            
         </MenuNavigator.Navigator>
+    );
+}
+
+const HomeNavigator = createStackNavigator();
+function MyHome() {
+    return (
+        <HomeNavigator.Navigator
+        initialRouteName='Home'
+        screenOptions={{
+            headerTintColor:'#fff',
+            headerStyle:{backgroundColor:'#512DA8'},
+            headerTitleStyle:{color:'#fff'}
+        }}>
+            <HomeNavigator.Screen name="Home" component={Home} 
+                options={{ title: 'Home' }}/>
+        </HomeNavigator.Navigator>
+    );
+}
+
+const MainNavigator = createDrawerNavigator();
+function MyDrawer() {
+    return (
+      <MainNavigator.Navigator 
+      drawerContentOptions={{
+        activeTintColor: '#e91e63',
+        itemStyle: { marginVertical: 5 },
+      }}>
+        <MainNavigator.Screen name='Menu' 
+            options={{drawerLabel: 'Menu'}} 
+            component={MyStack} />
+        <MainNavigator.Screen name='Home' 
+            options={{drawerLabel: 'Home'}} 
+            component={MyHome} />
+        
+        
+      </MainNavigator.Navigator>
     );
 }
 
@@ -28,7 +67,7 @@ class Main extends Component{
         return(
             <View style={{flex:1,paddingTop:Platform.OS==='ios'?0:StatusBar.currentHeight}} >
                 <NavigationContainer>
-                    <MyStack/>
+                    <MyDrawer />
                 </NavigationContainer>
             </View>
         )
