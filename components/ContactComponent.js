75f8099948fb24ea0ca6473f1e5fc186eccc6c60
Smriti Garg <smriti2111999@gmail.com>
2020-07-04 13:07:12 +0530

    Social Sharing

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -12,7 +12,7 @@ class Contact extends Component {
             body: 'To whom it may concern:'
         })
     }
-    
+
     render() {
         return(
             <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>  
@@ -23,7 +23,7 @@ class Contact extends Component {
                 <Text>Tel: +852 1234 5678</Text>
                 <Text>Email:confusion@food.net</Text>
                 <Text>Fax: +852 8765 4321</Text>
-                <Button
+                <Button 
                     title="Send Email"
                     buttonStyle={{backgroundColor: "#512DA8"}}
                     icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component, useRef } from 'react';
-import {View,Text, ScrollView, FlatList, Image, Button, Modal, StyleSheet, Alert, PanResponder} from 'react-native';
+import {View,Text, ScrollView, FlatList, Image, Button, Modal, StyleSheet, Alert, PanResponder, Share } from 'react-native';
 import {Card, Icon, Rating, Input } from 'react-native-elements'; 
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -59,6 +59,16 @@ function RenderDish(props){
         }
     })
 
+    const shareDish = (title, message, url ) => {
+        Share.share({
+            title: title,
+            message: title + ': ' + message + ' ' + url,
+            url: url
+        }, {
+            dialogTitle: 'Share '+title
+        });
+    }
+
     if(dish!=null){
         return(
         <Animatable.View animation='fadeInDown' duration={2000} delay={1000} 
@@ -81,6 +91,15 @@ function RenderDish(props){
                         type='font-awesome'
                         color='#512DA8'
                         onPress={() => props.onClick()}
+                        />
+                    <Icon 
+                        raised
+                        reverse
+                        name='share'
+                        type='font-awesome'
+                        color='#512DA8'
+                        //style={styles.cardItem}
+                        onPress={()=> shareDish(dish.name, dish.description, baseUrl+dish.image)}
                         />
                 </View>    
             </Card>
